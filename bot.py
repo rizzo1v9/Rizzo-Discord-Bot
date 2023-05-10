7f0c057b5cd1f342f74a2cb7b39959f6c68d880a
Carlo <cssanctuary1@gmail.com>
2023-05-09 20:06:18 -0400

    fixed spelling errors

--- bot.py
+++ bot.py
@@ -24,13 +24,12 @@ def run_discord_bot():
     @client.event
     async def on_message(message):
         if message.author == client.user:
-            return
 
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f"{username} sad: '{user_message}' ({channel}_")
+        print(f"{username} said: '{user_message}' ({channel})")
 
         if user_message[0] == '?':
             user_message = user_message[1:]
